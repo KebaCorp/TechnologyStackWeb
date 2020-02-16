dfe0f3054513bd5f1be5fcc9dbb185e792783be6
abek <vhkeba@gmail.com>
2020-02-16 23:51:34 +0300

    feat(Main): Home page redesign

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -4,90 +4,54 @@
     justify="center"
   >
     <v-col
-      cols="12"
-      sm="8"
-      md="4"
+      v-for="Type in technologies"
+      :key="Type.ID"
     >
       <v-card
-        v-for="(item, index) in payload"
-        :key="index"
-        class="mx-auto mb-5"
-        max-width="344"
-        outlined
+        :elevation="15"
       >
-        <technologies-list
-          :technologyType="item.type"
-          :technologies="item.technologies"
-        />
+        <v-card-title>
+          {{ Type.Title }}
+        </v-card-title>
+
+        <!--Stages-->
+        <v-card-text
+          v-for="Stage in Type.Stages"
+          :key="Stage.ID"
+        >
+
+          <!--Technologies-->
+          <v-list>
+            <v-subheader>{{ Stage.Title }}</v-subheader>
+            <v-list-item-group color="primary">
+              <v-list-item
+                v-for="Technology in Stage.Technologies"
+                :key="Technology.ID"
+              >
+                <v-list-item-content>
+                  <v-list-item-title class="text-left" v-text="Technology.Title"></v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list-item-group>
+          </v-list>
+
+        </v-card-text>
+
       </v-card>
     </v-col>
   </v-row>
 </template>
 
 <script>
-const TechnologiesList = () => import('./TechnologiesList')
+import { mapGetters } from 'vuex'
 
 export default {
   name: 'Main',
-  components: { TechnologiesList },
-  data: () => ({
-    payload: [
-      {
-        type: {
-          Title: 'Type1'
-        },
-        technologies: [
-          {
-            ID: 2,
-            TypeId: 1,
-            StageId: 1,
-            Title: 'Test',
-            IsDeprecated: false,
-            CreatorUserId: 1,
-            CreatedAt: '2020-02-16T19:44:24.364Z',
-            UpdatedAt: '2020-02-16T19:44:26.372Z'
-          },
-          {
-            ID: 3,
-            TypeId: 1,
-            StageId: 2,
-            Title: 'Test 2',
-            IsDeprecated: false,
-            CreatorUserId: 1,
-            CreatedAt: '2020-02-16T17:20:34.717949Z',
-            UpdatedAt: '2020-02-16T17:20:34.717949Z'
-          }
-        ]
-      },
-      {
-        type: {
-          Title: 'Type2'
-        },
-        technologies: [
-          {
-            ID: 2,
-            TypeId: 1,
-            StageId: 1,
-            Title: 'Test',
-            IsDeprecated: false,
-            CreatorUserId: 1,
-            CreatedAt: '2020-02-16T19:44:24.364Z',
-            UpdatedAt: '2020-02-16T19:44:26.372Z'
-          },
-          {
-            ID: 3,
-            TypeId: 1,
-            StageId: 2,
-            Title: 'Test 2',
-            IsDeprecated: false,
-            CreatorUserId: 1,
-            CreatedAt: '2020-02-16T17:20:34.717949Z',
-            UpdatedAt: '2020-02-16T17:20:34.717949Z'
-          }
-        ]
-      }
-    ]
-  }),
+  computed: {
+    ...mapGetters('technologies', [
+      'technologies'
+    ])
+  },
   created () {
     this.$store.dispatch('technologies/getTechnologies')
   }
